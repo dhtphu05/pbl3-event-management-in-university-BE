91ab56852bcabb515980042817ecb2d9c2ae304e
Phan Thanh Truong <lostpeanut123@gmail.com>
2025-06-06 00:23:48 +0700

    fix: minimal changes

---
+++ DUTEventManagementAPI/Migrations/20250605172309_Add title to TimeSlot.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DUTEventManagementAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddtitletoTimeSlot : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Title",
+                table: "TimeSlots",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Title",
+                table: "TimeSlots");
+        }
+    }
+}
